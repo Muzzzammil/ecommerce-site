43d485d1fd52db50e1a9eb5d7dfa5892728d5bc9
Muzammil's official <muzammilkhalid46@gmail.com>
2025-03-01 11:26:51 +0000

    Fixed merge conflict in index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-=======
 import 'bootstrap/dist/css/bootstrap.min.css'; // Keep this line for Bootstrap
->>>>>>> 544a8ce (Initial commit)
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
